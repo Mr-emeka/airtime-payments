ed8d851cce28a7ac34c4dd65e08e5d2954cd6a20
Mr-emeka <speak2c.emeka@gmail.com>
2020-06-19 06:25:21 +0100

    added firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   const url = "https://sandbox.wallets.africa/bills/airtime/purchase";
+  const publicKey = 'byog4b60fwz0'
+  const secretKey = 'g2h6yucvov83'
 
-  fetch(url)
-    .then((data) => {
-      return data.json();
-    })
-    .then((res) => {
-      console.log(res);
-    });
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    "Code": "airtel",
+    "Amount": 100,
+    "PhoneNumber": "07068260000",
+    "SecretKey": secretKey
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch(url, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
   const modal = document.querySelector('.modale');
   const open = () => {
